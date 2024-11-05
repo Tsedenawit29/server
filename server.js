94ed1f0e3798fe7b586902a0482cd24807fd677f
Tsedenya <135243199+Tsedenawit29@users.noreply.github.com>
2024-11-05 12:42:13 -0800

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const { Pool } = require('pg');
 const cors = require('cors');
 
 const app = express();
-const port = 5000; 
+const port = process.env.PORT || 5000; 
 
 // PostgreSQL connection
 const pool = new Pool({
