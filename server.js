f216dc3ab207af7e9e623573d30f1a261be36577
Tsedenya <135243199+Tsedenawit29@users.noreply.github.com>
2024-11-07 09:32:38 -0800

    Update server.js

--- server.js
+++ server.js
@@ -118,6 +118,6 @@ app.get('/songs', async (req, res) => {
   }
 });
 
-app.listen(port, () => {
+app.listen(port,'0.0.0.0', () => {
   console.log(`Server running on http://localhost:${port}`);
 });
